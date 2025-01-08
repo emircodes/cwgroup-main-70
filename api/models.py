80ba54c8331358d9d7cf650839b164be073d6652
firdausazfar <161317402+firdausazfar@users.noreply.github.com>
2025-01-08 21:35:18 +0000

    Update models.py
    add friend request

--- api/models.py
+++ api/models.py
@@ -49,3 +49,15 @@ class Hobby(models.Model):
     def __str__(self):
         return self.name
 
+class FriendRequest(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_request')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_request')
+    status = models.CharField(
+        max_length=10,
+        choices=[('pending', 'Pending'), ('accepted', 'Accepted')],
+        default = 'pending'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.sender.email} -> {self.receiver.email} ({self.status})"
