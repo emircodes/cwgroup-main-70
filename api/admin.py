3994b10240f0abdc8b011c8bb12827ea18b078a9
firdausazfar <161317402+firdausazfar@users.noreply.github.com>
2025-01-10 13:35:05 +0000

    admin.py
    add register noti and friend req

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import User, Hobby
+from .models import User, Hobby, FriendRequest, Notification
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Hobby)
+admin.site.register(FriendRequest)
+admin.site.register(Notification)
