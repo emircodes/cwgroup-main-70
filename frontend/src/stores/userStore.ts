a8b8045d38b82ccd2bc352003dfac3d648a6f593
zedchuu <adam.zikri.b.a@gmail.com>
2025-01-14 03:00:26 +0800

    Table sorted by Similarity scores
    This is an early implementation. Functionality will be added later on
    +Added similarity_score to serializer, urls.py, and UserList.vue
    +Added new function similar_users, that get user current hobbies and sort  order by most similar
    +Changed userStore.ts to cater to similarUsers

--- frontend/src/stores/userStore.ts
+++ frontend/src/stores/userStore.ts
@@ -1,23 +1,19 @@
-// src/stores/userStore.ts
 import { defineStore } from 'pinia';
 import axios from 'axios';
 
 export const useUserStore = defineStore('userStore', {
-    state: () => ({
-      users: [] as Array<{ id: number; username: string; email: string; name: string }>
-    }),
-    actions: {
-      async fetchUsers() {
-        console.log("Fetching users from store...");
-        try {
-          const response = await axios.get('/api/users/');
-          console.log("Store API Response:", response.data);
-          this.users = response.data;
-          console.log("Users stored in Pinia state:", this.users);
-        } catch (error) {
-          console.error("Error fetching users in store:", error);
-        }
+  state: () => ({
+    similarUsers: [] as Array<{ id: number; username: string; email: string; name: string; similarity_score: number; }>,
+  }),
+  actions: {
+    async fetchSimilarUsers() {
+      try {
+        const response = await axios.get('/api/similar-users/');
+        console.log("API Response in Pinia:", response.data);
+        this.similarUsers = response.data;
+      } catch (error) {
+        console.error("Error fetching similar users:", error);
       }
-    }
-  });
-  
+    },
+  },
+});
